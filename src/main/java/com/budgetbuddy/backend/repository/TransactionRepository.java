fc2dec31325c5816fb6ce309b7596a114f6dcc66
Manas-Nanivadekar <manasnanivadekar@gmail.com>
2023-10-03 22:54:08 +0530

    update category

--- src/main/java/com/budgetbuddy/backend/repository/TransactionRepository.java
+++ src/main/java/com/budgetbuddy/backend/repository/TransactionRepository.java
@@ -1,9 +1,11 @@
 package com.budgetbuddy.backend.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.budgetbuddy.backend.model.Transaction;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-
+    Optional<Transaction> findById(Long id);
 }
